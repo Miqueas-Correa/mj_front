fce29113c3407addbf7576cbe325577bf4a62dd8
Miqueas Correa <miki.mate.tizi@gmail.com>
2026-01-15 23:14:20 -0300

    prueba para vercel

--- mi-frontend/src/hooks/useProductos.js
+++ mi-frontend/src/hooks/useProductos.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import client from "../services/client";
 
 export function useProductos(mostrar = true, extraRoute = "") {
+  console.log("API BASE:", import.meta.env.VITE_API_BASE_URL);
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
